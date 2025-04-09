f081d38fe04e2d88495debd95a1f7e7eaed817b9
brookedonnellyward <bdonnelly4015@gmail.com>
2025-04-08 19:17:37 -0500

    adding another message to main

--- test/src/test/Main.java
+++ test/src/test/Main.java
@@ -6,6 +6,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
 		System.out.println("Testing making change from Github");
+		System.out.println("Testing making change from eclipse");
 		}
 
 }
